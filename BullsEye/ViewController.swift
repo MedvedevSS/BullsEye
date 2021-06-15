408a1cbba393d4c1c707cd1a6864e50dcc4e8c53
devGod <devgod@mail.ru>
2021-06-15 16:26:51 +0300

    added elements

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var currentValue = 0
+    var targetValue = 0
+    
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        starNewRound()
+    }
+    
+    func starNewRound() {
+        
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        
+        targetLabel.text = "\(targetValue)"
+    }
+    
+    @IBAction func showAlert() {
+        
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
+        
+        let alert = UIAlertController(title: "Hello World",
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK",
+                                   style: .default,
+                                   handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        starNewRound()
     }
 
-
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    
 }
 
